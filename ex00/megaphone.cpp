4958d805ffab656721f1c4bb6479e3f098a9c099
Ivan Bettencourt <ibettenc@fu-c4-p5.s19.be>
2026-02-19 16:18:10 +0100

    2

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,26 +6,27 @@
 /*   By: ibettenc <ibettenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 11:17:45 by ibettenc          #+#    #+#             */
-/*   Updated: 2026/02/17 12:07:56 by ibettenc         ###   ########.fr       */
+/*   Updated: 2026/02/19 13:41:54 by ibettenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include <cctype>
+using namespace std;
 
 int main(int ac, char **av)
 {
     if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<std::endl;
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
     else
     {
         for (int i = 1; av[i]; i++)
         {
             for (int j = 0; av[i][j]; j++)
-                std::cout << static_cast<char>(std::toupper(av[i][j]));
+                cout << static_cast<char>(toupper(av[i][j]));
         }
-        std::cout << std::endl;
+        cout << endl;
     }        
     return 0;
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,50 +6,14 @@
 /*   By: ibettenc <ibettenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 12:10:08 by ibettenc          #+#    #+#             */
-/*   Updated: 2026/02/17 14:39:36 by ibettenc         ###   ########.fr       */
+/*   Updated: 2026/02/19 14:37:35 by ibettenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-
-class PhoneBook
-{
-    private:
-        Contact contacts[8];
-        int count;
-        int oldest;
-            
-    public:
-        PhoneBook()
-        {
-            count = 0;
-            oldest = 0;
-        }
-        ADD()
-        {
-            // - save a new contact
-            // if 8 contacts create a new one and delete the oldest
-            // - ask an input for each parameter
-            // a saved contact cannot have empty field
-        }
-        SEARCH()
-        {
-            // - display a specific contact
-            //  Display the saved contacts as a list of 4 columns: index, first name, last
-            //     name and nickname.
-            // ◦ Each column must be 10 characters wide. A pipe character (’|’) separates
-            // them. The text must be right-aligned. If the text is longer than the column,
-            // it must be truncated and the last displayable character must be replaced by a dot (’.’).
-            // ◦ Then, prompt the user again for the index of the entry to display. If the index
-            // is out of range or wrong, define a relevant behavior. Otherwise, display the
-            // contact information, one field per line
-        }
-        EXIT()
-        {
-            // exit the program and DONT SAVE the contacts
-        }
-};
+#include <iomanip>
+using namespace std;
 
 class Contact
 {
@@ -104,14 +68,105 @@ class Contact
         }
 };
 
+class PhoneBook
+{
+    private:
+        Contact contacts[8];
+        int count;
+        int oldest;
+            
+    public:
+        PhoneBook()
+        {
+            count = 0;
+            oldest = 0;
+        }
+        void ADD()
+        {
+            string first_name;
+            string last_name;
+            string nickname;
+            string phone_number;
+            string darkest_secret;
+            
+            cout << "First name : ";
+            cin >> first_name;
+
+            cout << "Last_name : ";
+            cin >> last_name;
+
+            cout << "Nickname : ";
+            cin >> nickname;
+
+            cout << "Phone number : ";
+            cin >> phone_number;
+
+            cout << "Darkest secret : ";
+            cin >> darkest_secret;
+            
+            Contact new_contact(first_name, last_name, nickname, phone_number, darkest_secret);
+            contacts[oldest] = new_contact;
+
+            if (count < 8)
+                count++;
+            
+            oldest++;
+            
+            if (oldest == 8)
+                oldest = 0;
+        }
+        void SEARCH()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                cout << i;
+                cout << " | ";
+                cout << contacts[i].get_first_name();
+                cout << " | ";
+                cout << contacts[i].get_last_name();
+                cout << " | ";
+                cout << contacts[i].get_nickname();
+                cout << endl;
+            }
+            
+
+            // ◦ Each column must be 10 characters wide. A pipe character (’|’) separates
+            // them. The text must be right-aligned. If the text is longer than the column,
+            // it must be truncated and the last displayable character must be replaced by a dot (’.’).
+            // ◦ Then, prompt the user again for the index of the entry to display. If the index
+            // is out of range or wrong, define a relevant behavior. Otherwise, display the
+            // contact information, one field per line
+        }
+        void EXIT()
+        {
+            // exit the program and DONT SAVE the contacts
+        }
+};
+
+
+
 
 int main()
 {
-    Contact alice("Alice", "0456356220");
+    PhoneBook phonebook;
+    string command;
+    
+    while (true)
+    {
+        cout << ": ";
+        cin >> command;
+        
+        if (command == "EXIT")
+            break;
+        else if (command == "SEARCH")
+            phonebook.SEARCH();
+        else if (command == "ADD")
+            phonebook.ADD();
+    }
 }
 
-V 1 Crée la classe Contact avec ses attributs et getters/setters
-V 2 Crée la classe PhoneBook avec le tableau fixe 
-3 Code la commande ADD
-4 Code la commande SEARCH
-5 Gère la boucle principale dans main()
+// V 1 Crée la classe Contact avec ses attributs et getters/setters
+// V 2 Crée la classe PhoneBook avec le tableau fixe 
+// 3 Code la commande ADD
+// 4 Code la commande SEARCH
+// 5 Gère la boucle principale dans main()
